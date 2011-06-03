976106a5b2e4558995a5121797d9c2d9fc803348
Karl Jonathan Ward <karl.j.ward@gmail.com>
2011-06-03 14:05:31 +0100

    lib/rdf.rb: Rough outline of pdf spatial object parsing DSL.

---
+++ TODO.md
@@ -0,0 +1,22 @@
+# TODO
+
+## Tasks with unclassified time period
+
+* DONE Pass receiver through to spatials calls, or construct receiver
+  from method calls in spatials calls.
+* Write methods to create spatial objects.
+* DONE Use show_text_with_positioning to construct text runs.
+* Record spatial objects that result from spatials method calls.
+* Complete the covert method - write spatial hashs out to xml.
+* DONE Write Spatial class with alter method.
+
+## Small tasks, 10 minutes or so
+
+
+
+## Tasks, up to 3 hours
+
+
+
+## Long tasks, greater than 3 hours
+
---
+++ lib/pdf.rb
@@ -0,0 +1,172 @@
+require 'pdf-reader'
+
+# A DSL that aids in developing an understanding of the spatial
+# construction of PDF pages.
+
+class SpatialObject < Hash
+
+  def operations
+    @operations ||= {
+      :grow_by => lambda { |original, modifier| original + modifier },
+      :grow_by_percent => lambda { |original, pcnt| original * (1 + pcnt) },
+      :shrink_by => lambda { |original, modifier| original - modifier },
+      :shrink_by_percent => lambda { |original, pcnt| original * (1 - pcnt) },
+      :set_to => lambda { |original, new| new },
+      :set_to_percent => lambda { |original, pcnt| original * pcnt },
+      :with => lambda { |original, p| p.call(original) }
+    }
+  end
+  
+  def alter op_schema
+    op_schema.each_pair do |param, operations|
+      operations.each_pair do |operation, value|
+        self[param] = self.operations[operation].call(self[param], value)
+      end
+    end
+  end
+  
+end
+
+class Receiver
+
+  def initialize pdf
+    @pdf = pdf
+    @listeners = {}
+  end
+
+  def for callback_name, &block
+    @listeners[callback_name] = {:type => @pdf.operating_type, :fn => block}
+  end
+
+  def expand_listeners_to_callback_methods
+    # TODO
+    # for each in listeners
+    #   create callback method if it doesn't exist that
+    #   looks up callback_name in listeners, executes each
+    #   fn with data from the callback, and adds the result
+    #   to spatial type :type.
+  end
+
+end
+
+class Pdf
+  
+  attr_accessor :operating_type, :spatial_calls, :spatial_builders
+  
+  def method_missing name, *args
+    throw StandardError.new "No such spatial type #{name}"
+  end
+
+  def spatials name, options = {}, &block
+    @operating_type = name
+    add_spatials_method name, options, &block
+  end
+
+  def initialize
+    @spatial_builders = {}
+    @spatial_calls = []
+    @spatial_objects = {}
+    
+    self.spatials :text_runs do |parser|
+      parser.for :show_text_with_positioning do |data|
+        # TODO Make SpatialObjects
+        # TODO How are they added to @spatial_objects?
+        puts o
+      end
+    end
+
+    self.spatials :images do
+      parser.for :begin_inline_image_data do |data|
+      end
+    end
+    
+    self.spatials :v_margins, :depends_on => [:text_runs] do
+      # Mark off ranges of the x axis. AxisMask class?
+    end
+    
+    self.spatials :h_margins, :depends_on => [:text_runs] do
+    end
+    
+    self.spatials :rows, :depends_on => [:h_margins] do
+    end
+    
+    self.spatials :columns, :depends_on => [:v_margins, :rows] do
+    end
+    
+    self.spatials :regions, :depends_on => [:text_runs] do
+    end
+    
+    self.spatials :sections, :depends_on => [:groups, :columns] do
+    end
+  end
+
+  private
+  
+  def add_spatials_method name, options={}, &block
+    @spatial_builders[name] = block
+    @spatial_objects[name] = []
+
+    p = Proc.new do
+      # TODO Check for missing depends_on in the spatials_calls stack.
+      
+      @spatial_calls << {
+        :name => name,
+        :explicit => true
+      }
+      
+      @spatial_objects[name].each do |o|
+        yield o
+      end
+    end
+
+    self.class.send :define_method, name, p
+  end
+end
+
+def parse filename, &block
+  pdf = Pdf.new
+  yield pdf
+  
+  receiver = Receiver.new pdf
+  pdf.spatial_calls.each do |spatial_call|
+    pdf.spatial_builders[spatial_call[:name]].call receiver
+  end
+
+  PDF::Reader.new filename, receiver
+  
+  pdf
+end
+
+def view filename, options = {}, &block
+  pdf = parse filename, &block
+
+  # TODO For each called object type that was explicity called,
+  # render it to an image.
+end
+
+def convert filename, options = {}, &block
+  pdf = parse filename, &block
+
+  # TODO For each called object type, iterate over its spatial
+  # objects and output to the type specified by :to.
+end
+
+# Usage
+
+view "/home/karl/some.pdf" do |pdf|
+  pdf.text_runs do |run|
+    double_height = {
+      :height => {:grow_by_percent => 1},
+      :y => {:shrink_by => run[:height] / 2}
+    }
+    run.alter double_height
+  end
+
+  pdf.sections
+end
+
+convert "/home/karl/some.pdf", :to => :xml do |pdf|
+  pdf.text_runs
+  pdf.sections
+end
+

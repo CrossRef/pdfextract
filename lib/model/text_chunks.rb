33a59f5709853983b712600c3c8d81e542d903b4
Karl Jonathan Ward <karl.j.ward@gmail.com>
2011-06-16 13:52:54 +0100

    text_chunks.rb: If chunks overlap on the y axis by more than 90%, merge.

--- lib/model/text_chunks.rb
+++ lib/model/text_chunks.rb
@@ -7,12 +7,13 @@ module PdfExtract
     def self.include_in pdf
       char_slop = 0.2
       word_slop = 1.5
+      vertical_overlap_factor = 0.9
       
       pdf.spatials :text_chunks, :depends_on => [:characters] do |parser|
         rows = {}
         parser.objects :characters do |chars|
           # TODO Handle pages.
-          y = chars[:y].floor
+          y = chars[:y]
           rows[y] = [] if rows[y].nil?
 
           idx = rows[y].index { |obj| chars[:x] <= obj[:x] }
@@ -28,7 +29,7 @@ module PdfExtract
 
           rows.each_pair do |y, row|
             char_width = row.first[:width]
-            
+
             while row.length > 1
               left = row.first
               right = row[1]
@@ -65,8 +66,39 @@ module PdfExtract
 
             text_chunks << row.first
           end
+
+          # Merge chunks that have slightly different :y positions but which
+          # mostly overlap in the y dimension.
+
+          text_chunks.sort_by! { |obj| obj[:x] }
+          merged_text_chunks = []
+
+          while text_chunks.count > 1
+            left = text_chunks.first
+            right = text_chunks[1]
+
+            overlap = [left[:height], right[:height]].min - (left[:y] - right[:y]).abs
+            overlap = overlap / [left[:height], right[:height]].min
+
+            if overlap >= vertical_overlap_factor
+              # join
+              so = SpatialObject.new
+              so[:x] = left[:x]
+              so[:y] = left[:y]
+              so[:width] = (right[:x] - left[:x]) + right[:width]
+              so[:height] = [left[:height], right[:height]].max
+              so[:content] = left[:content] + right[:content]
+
+              text_chunks[0] = so
+              text_chunks.delete_at 1
+            else
+              # no join
+              merged_text_chunks << text_chunks.first
+              text_chunks.delete_at 0
+            end
+          end
           
-          text_chunks
+          merged_text_chunks << text_chunks.first
         end 
       end
     end

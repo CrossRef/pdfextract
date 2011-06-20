2ee1e74e539055e83bc0506d55991df80e7e2840
Karl Jonathan Ward <karl.j.ward@gmail.com>
2011-06-20 12:55:02 +0100

    png_view.rb: Fix color assignment.

--- lib/view/png_view.rb
+++ lib/view/png_view.rb
@@ -12,7 +12,7 @@ module PdfExtract
         color = auto_color
         objs.each do |obj|
           gc = Magick::Draw.new
-          gc.fill = color
+          gc.fill = "\##{color}"
           gc.rectangle(obj[:x], obj[:y], obj[:x] + obj[:width],
                        obj[:y] + obj[:height])
           gc.draw img

d36f5863db22d441d1f311dbd3b682552a1ed2ff
Karl Jonathan Ward <karl.j.ward@gmail.com>
2011-07-20 11:38:25 +0100

    catalog.rb: Working version.

--- test/catalog.rb
+++ test/catalog.rb
@@ -9,28 +9,58 @@
 require 'cgi'
 require 'net/http'
 require 'uri/http'
+require 'commander/import'
+require 'nokogiri'
+require 'json'
 
-def query_uri prefix, issue, year
+def query_uri verb, prefix, issue, year
   URI::HTTP.build({
     :host => "oai.crossref.org",
     :path => "/OAIHandler",
     :query => {
-      :verb => "ListIdentifiers",
-      :metadataPrefix => "cr_unixml"
+      :verb => verb,
+      :metadataPrefix => "cr_unixml",
+      :set => "#{prefix}:#{issue}:#{year}"                
     }
   })
 end
 
-def 
+def parse_dois xml
+  doc = Nokogiri::XML::Document.new xml
+  identifiers = doc.xpath "//identifier"
+  identifiers.map { |id| id.text }
+end
 
+def get_dois prefix, issue, year
+  uri = query_uri "ListIdentifiers", prefix, issue, year
 
-  
-Net::HTTP.start "oai.crossref.org" do |http|
+  Net::HTTP.start uri.host do |http|
+    response = http.get uri.request_uri
 
-  URI::HTTP.build {
-    :path => "/OAIHandler",
-    :query => {
-      :verb => "ListIdentifiers",
-      :metadataPrefix => "cr_unixml"
-    }
-  }
+    if response == 200
+      parse_dois response.body
+    else
+      fail "Failed to get metadata. OAI server returned: #{response.code}"
+    end
+  end
+end
+
+command :populate do |c|
+  c.syntax = "catalog populate publisher_prefix:journal_id:year"
+  c.description = "Add Crossref Metadata to a catalog."
+
+  c.acton do |args, options|
+    args.each do |limiting_set|
+      dois = get_dois limiting_set.split(":")
+      say dois.to_json
+    end
+  end
+end
+
+command :download do |c|
+  c.syntax = "catalog download"
+  c.description = "Locate and download PDFs for DOIs in a catalog."
+
+  c.action do |args, options|
+  end
+end

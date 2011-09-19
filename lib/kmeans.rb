5c281a93981cd50f4f8b6d337ac6db3c87681ef3
Karl Jonathan Ward <karl.j.ward@gmail.com>
2011-09-19 16:55:45 +0100

    kmeans.rb: n-dimensional k-means algorithm for bare hashes.
    Will be used to cluster sections on :letter_ratio, :cap_ratio,
    :year_ratio and :name_ratio to determine potential reference sections.

---
+++ lib/kmeans.rb
@@ -0,0 +1,94 @@
+module PdfExtract
+  module Kmeans
+
+    def self.take_keys item, keys
+      r = {}
+      keys.each do |key|
+        r[key] = item[key]
+      end
+      r
+    end
+
+    def self.ndist a, b, keys
+      sum = 0
+      keys.each do |key|
+        sum += (a[key] - b[key]) ** 2
+      end
+      Math.sqrt sum
+    end
+
+    def self.cluster_centre cluster
+      keys = cluster[:centre].keys
+
+      centre = {}
+
+      # Sum each key
+      cluster[:items].each do |item|
+        keys.each do |key|
+          centre[key] ||= 0
+          centre[key] += item[key]
+        end
+      end
+
+      # Avg each key
+      centre.each_key do |key|
+        centre[key] = centre[key] / cluster[:items].length.to_f
+      end
+
+      centre
+    end
+  
+    def self.clusters items, keys, options = {}
+      options = {:k => 5, :delta => 0.001}.merge options
+
+      # Make k clusters with random centre points
+      cs = []
+      k.times do
+        idx = (items.length * rand).to_i
+        cs << {:centre => take_keys(items[idx], keys), :items => []}
+      end
+
+      while true
+        
+        # Add each item to a cluster
+        items.each do |item|
+          min_distance = +INFINITY
+          selected_cluster = nil
+
+          cs.each do |cluster|
+            distance = ndist(item, cluster[:centre], keys)
+            if distance < min_distance
+              min_distance = distance
+              selected_cluster = cluster
+            end
+          end
+
+          selected_cluster[:items] << item
+        end
+
+        max_delta = -INFINITY
+
+        # Recalculate centre points and max delta
+        cs.each do |cluster|
+          old_centre = cluster[:centre]
+          centre = cluster_centre cluster
+          cluster.merge! {:centre => centre}
+
+          max_delta = [ndist(old_centre, centre, keys), max_delta].max
+        end
+        
+        if max_delta <= options[:delta]
+          break
+        else
+          cs.each do |cluster|
+            cluster[:items] = []
+          end
+        end
+       
+      end
+
+      cs
+    end
+
+  end
+end

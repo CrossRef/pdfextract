dcdcf73e5f1e605d8749a1ed4cdff30c3d1a2e82
Karl Jonathan Ward <karl.j.ward@gmail.com>
2012-01-23 10:16:21 +0000

    chunks.rb: Bunch of bug fixes.

--- lib/model/chunks.rb
+++ lib/model/chunks.rb
@@ -5,23 +5,25 @@ module PdfExtract
 
     # TODO Look for obj[:writing_mode] == :vertical or :horizontal
 
-    Settings.declare :word_slop, {
-      :default => 4.0,
+    Settings.declare :min_word_spacing, {
+      :default => 0.01,
       :module => self.name,
-      :description => "Maximum allowed space between words for them to be considered part of the same line. word_slop is multiplied by width of the last character in a word to find its joining width."
+      :description => "Minimum distance between characters, as a factor of character width, for characters to be cosidered as belonging to separate words."
     }
 
     def self.select_container containers, chars
-      containers.reject { |c| not Spatial.contains? c[:container], chars }.first
+      containers.reject { |c|
+        not Spatial.contains? c[:container], chars
+      }.first
     end
 
     def self.include_in pdf
       deps = [:characters, :headers, :footers, :columns]
       pdf.spatials :chunks, :paged => true, :depends_on => deps do |parser|
-        containers = {}
+        containers = []
 
         parser.before do
-          containers = {}
+          containers = []
         end
 
         parser.objects :headers do |header|
@@ -45,29 +47,33 @@ module PdfExtract
           }
         end
           
-        parser.objects :characters do |chars|
-          c = select_container containers, chars
-          
-          found_chunk = false
-          c[:chunks].each do |chunk|
-            if Spatial.overlap? :y, :height, chunk, chars
-              chunk[:chars] << chars
-              chunk[:y] = [chars[:y], chunk[:y]].min                # these are wrong
-              chunk[:height] = [chars[:height], chunk[:height]].max # --/
-            end
-          end
+        parser.objects :characters do |char|
+          c = select_container containers, char
 
-          if not found_chunk
-            c[:chunks] << {
-              :y => chars[:y],
-              :height => chars[:height],
-              :chars => [chars]
-            }
+          if not c.nil?
+            found_chunk = false
+            c[:chunks].each do |chunk|
+              if Spatial.overlap? :y, :height, chunk, char
+                chunk[:chars] << char
+                chunk[:y] = [char[:y], chunk[:y]].min                
+                chunk[:height] = [char[:height], chunk[:height]].max # this is wrong
+                found_chunk = true
+                break
+              end
+            end
+            
+            if not found_chunk
+              c[:chunks] << {
+                :y => char[:y],
+                :height => char[:height],
+                :chars => [char]
+              }
+            end
           end
         end
 
         parser.after do
-          word_slop = pdf.settings[:word_slop]
+          min_word_spacing = pdf.settings[:min_word_spacing]
           chunk_objs = []
           
           containers.each do |container|
@@ -77,16 +83,17 @@ module PdfExtract
 
               # merge char content into the first char object
               while chunk[:chars].length > 1
+                
                 left = chunk[:chars].first
                 right = chunk[:chars][1]
 
-                if left[:x] + left[:width] + (left[:width] * word_slop) <= right[:x]
+                if left[:x] + left[:width] + (left[:width] * min_word_spacing) <= right[:x]
                   left[:content] += " " + right[:content]
                 else
                   left[:content] += right[:content]
                 end
                 
-                left[:width] += right[:width]
+                left[:width] = (right[:x] + right[:width]) - left[:x]
 
                 chunk[:chars].delete_at 1
               end
@@ -97,6 +104,7 @@ module PdfExtract
                 :y => chunk[:y],
                 :height => chunk[:height]
               })
+
             end
           end
 
